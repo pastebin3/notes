930c08992a9617ba438f0ab0212d00f362a04f0e
multimedia22x@gmail.com <multimedia22x@gmail.com>
2024-09-16 10:19:05 +0200

    one more time

--- note.sh
+++ note.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+
+# some variables
 note_name="$(date +%d-%m-%Y)"
 note_title="$(date +%A,' '%d/%m/%Y)"
 note_time="$(date +%H:%M)"
+# note name and directory
 my_note="$HOME/notes/${note_name}.md"
-
+# add a title to note if it doesn't exist
 [ ! -f ${my_note} ] && echo "## Your notes for: ${note_title}" >${my_note}
-
+# note layout
 layout=(
   "normal G2o"
   "normal i## ${note_time}"
@@ -13,7 +16,7 @@ layout=(
   "normal zz"
   "startinsert"
 )
-
+# edit with your fave editor
 nvim "${layout[@]/#/+}" ${my_note}
 
 # make copies
