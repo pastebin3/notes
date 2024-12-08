67e958ce5a179fa549a6d1d5ef472c938e991ab5
multimedia22x@gmail.com <multimedia22x@gmail.com>
2024-12-08 12:53:41 +0100

    latest

--- note.sh
+++ note.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
+
 # this script was created by dj - multimedia22.be
 # it's also meant to work with lazyvim
 # a follow up tutorial will be made available soon
 # let's begin by creating some variables
+
 note_name="$(date +%d-%m-%Y)"
 note_title="$(date +%A,' '%d/%m/%Y)"
 note_time="$(date +%H:%M)"
+
 # note name and directory
 my_note="$HOME/notes/gh/notes/${note_name}.md"
+
 # add a title to note if it doesn't exist
 [ ! -f ${my_note} ] && echo "## Your notes for: ${note_title}" >${my_note}
+
 # note layout
 layout=(
   "normal G2o"
@@ -18,7 +23,9 @@ layout=(
   "normal zz"
   "startinsert"
 )
+
 # edit with your fave editor
 nvim "${layout[@]/#/+}" ${my_note}
+
 # make copies with diff extensions/locations
 tee ${note_name}.pdf ${note_name}.txt <${my_note} >/dev/null
